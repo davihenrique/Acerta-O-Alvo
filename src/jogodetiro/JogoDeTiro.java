01cf0eab21d7b46faa23eca213bd6d2f6e0a46c8
aluno <aluno@UCRC635.UC.ACAD>
2017-06-09 18:27:39 -0300

    Remocao da classe VerificarVitoria por FimdeJogo

--- src/jogodetiro/JogoDeTiro.java
+++ src/jogodetiro/JogoDeTiro.java
@@ -1,17 +1,17 @@
-
 package jogodetiro;
+
 import jogodetiro.TiroAoAlvo;
 
+/**
+* Davi Henrique dos Santos Silva   2016100588
+* Mateus Marques da Costa          2016101068
+* Victor Oliveira Fonseca de Melo  2016100925
+* ...
+*/
 public class JogoDeTiro {
    
     public static void main(String[] args) {
-       
-        TiroAoAlvo jogo = new TiroAoAlvo(20,5);
-        
-        jogo.Joga("João");        
-        
-   
-           
+        TiroAoAlvo jogo = new TiroAoAlvo(20,3);
+        jogo.Joga("João");           
     }
-   
 }
--- src/jogodetiro/TiroAoAlvo.java
+++ src/jogodetiro/TiroAoAlvo.java
@@ -5,122 +5,116 @@ import static java.lang.Math.sqrt;
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- *
- * @author davih
- */
-class TiroAoAlvo{
-   private int tiros;
-   private final int niTiros;
-   private int idTiro = 1;
-   private  String nomeJogador;
-   private final int tamCampo;
-   private int alvoX;  private int alvoY;
-   private int pontoX;  private int pontoY;
-   private float pontos;
-   private final float valorPontoMax = 100;
+class TiroAoAlvo {
 
+    private int tiros;
+    private final int niTiros;
+    private int idTiro = 1;
+    private String nomeJogador;
+    private final int tamCampo;
+    private int alvoX;
+    private int alvoY;
+    private int pontoX;
+    private int pontoY;
+    private float pontos;
+    private final float valorPontoMax = 100;
 
     public TiroAoAlvo(int quadrado, int tiros) {
         this.tamCampo = quadrado;
         this.tiros = tiros;
         this.niTiros = tiros;
     }
+
     public void Joga(String nome) {
         this.nomeJogador = nome;
         Apresentar();
         RandomAlvo();
-        while(this.tiros > 0 ){
-            
+        while (this.tiros > 0) {
             Atirar();
-            VerificaVitoria();
-            
+            FimDeJogo();
         }
     }
-    private void Apresentar(){
-        System.out.println("Jogador "+nomeJogador);
-        System.out.println("Seu alvo está no intervalo ["+(-1*tamCampo)+","
-                           + "+"+tamCampo+"]");
-        System.out.println("Você tem direito a "+tiros+" tiros");
-        
-    }
-    private void Atirar(){
 
-        System.out.println("Tiro "+(this.idTiro)+" - entre com o par x y: ");
+    private void Apresentar() {
+        System.out.println("===================================================");
+        System.out.println("Jogador " + this.nomeJogador);
+        System.out.println("Seu alvo está no intervalo [" + (-1 * this.tamCampo)
+                + ","+ "+" + this.tamCampo + "]");
+        System.out.println("Você tem direito a " + tiros + " tiros");
+        System.out.println("===================================================");
+    }
+
+    private void Atirar() {
+        System.out.println("Tiro " + (this.idTiro) + " - entre com o par x y: ");
         Scanner ler = new Scanner(System.in);
-        pontoX = ler.nextInt();
+        this.pontoX = ler.nextInt();
         ler = new Scanner(System.in);
-        pontoY = ler.nextInt();
-       
-        if(pontoX <= tamCampo && pontoY <= tamCampo && pontoX >= (-1*tamCampo) && pontoY >= (-1*tamCampo)){
-        idTiro++;
-        Nota();
-        
-        }else{
+        this.pontoY = ler.nextInt();
+        if (this.pontoX <= this.tamCampo && 
+                this.pontoY <= this.tamCampo && 
+                this.pontoX >= (-1 * this.tamCampo) && 
+                this.pontoY >= (-1 * this.tamCampo)) {
+            this.idTiro++;
+            Nota();
+        } else {
             System.out.println("Erro, Área digitada invalida, tente novamente");
             Atirar();
         }
-        
-        
-       
-    
-}
-    private void Nota(){
+    }
+
+    private void Nota() {
         float d;
-        
-        if(pontoX == alvoX && pontoY == alvoY ){
-            
-            pontos+= 100;
-        }else{
-            d=Distancia(pontoX,pontoY,alvoX,alvoY);
-            
-            System.out.println("PONTOS ANTES"+this.pontos);
-            if(d < (tamCampo*5)/100){
-                pontos+= valorPontoMax/(niTiros-1);
-            }else if (d < (tamCampo*20)/100){ 
-                pontos+=valorPontoMax/(niTiros*2);
-            }else if (d < (tamCampo*50)/100) {
-                pontos+=valorPontoMax/(niTiros*3);
-            }else if(d < (tamCampo*80)/100){
-                pontos+=valorPontoMax/(niTiros*4);
+        if (this.pontoX == this.alvoX && this.pontoY == this.alvoY) {
+            this.pontos += 100;
+        } else {
+            d = Distancia(this.pontoX, this.pontoY, this.alvoX, this.alvoY);
+            //System.out.println("PONTOS ANTES"+this.pontos);
+            if (d < (tamCampo * 5) / 100) {
+                this.pontos += this.valorPontoMax / (this.niTiros - 1);
+            } else if (d < (tamCampo * 20) / 100) {
+                this.pontos += this.valorPontoMax / (this.niTiros * 2);
+            } else if (d < (tamCampo * 50) / 100) {
+                this.pontos += this.valorPontoMax / (this.niTiros * 3);
+            } else if (d < (tamCampo * 80) / 100) {
+                this.pontos += this.valorPontoMax / (this.niTiros * 4);
             }
-            System.out.println("PONTOS DEPOIS"+this.pontos);
+            //System.out.println("PONTOS DEPOIS"+this.pontos);
         }
-   
     }
-    private void VerificaVitoria(){
-        if(pontos >= valorPontoMax){
+
+    private void FimDeJogo() {
+        if (pontos >= this.valorPontoMax) {
             this.pontos = 100;
-            System.out.println("Arcetou O Alvo faltando "+(this.niTiros - this.idTiro+1)+" tiros");
+            System.out.println("Arcetou O Alvo faltando " + 
+                    (this.niTiros - this.idTiro + 1) + " tiros");
             System.out.println("VC GANHOU");
-            System.out.println("Seus pontos: "+pontos);
-             System.out.println("Seus pontos: "+pontos);
+            System.out.println("Seus pontos: " + this.pontos);
+            System.out.println("===================================================");
             tiros = 0;
-        }else{
+        } else if (this.tiros > 1) {
             this.tiros--;
-            System.out.println("Seus pontos: "+pontos);
+            System.out.println("Seus pontos: " + this.pontos);
+        } else {
+            this.tiros--;
+            System.out.println("Fim do Jogo.");
+            System.out.println("Jogador " + this.nomeJogador + 
+                    ", seu total foi de " + this.pontos + " pontos.");
         }
-        
+
     }
-    private void RandomAlvo(){
-    
+
+    private void RandomAlvo() {
         Random num = new Random();
-        
-        alvoX = -1*tamCampo + num.nextInt(tamCampo+tamCampo)+1;
-       
+        this.alvoX = -1 * this.tamCampo + num.nextInt(this.tamCampo+
+                this.tamCampo) + 1;
         num = new Random();
-        
-        alvoY = -1*tamCampo + num.nextInt(tamCampo+tamCampo)+1;
-        
-        System.out.println(alvoX+" "+alvoY);
-}
-    private float Distancia(int xb, int yb,int xa, int ya){
+        alvoY = -1 * this.tamCampo + num.nextInt(this.tamCampo+this.tamCampo)+1;
+        System.out.println(this.alvoX + " " + this.alvoY);
+    }
+
+    private float Distancia(int xb, int yb, int xa, int ya) {
         float d;
-        
-        d =  (float) sqrt(((int) pow((xb-xa),2))+((int) pow((yb-ya),2)));
-       
-
+        d = (float) sqrt(((int) pow((xb - xa), 2)) + ((int) pow((yb - ya), 2)));
         return d;
-
-     }
+    }
 }
