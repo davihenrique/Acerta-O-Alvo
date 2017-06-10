948a2345e04e85af07be920886d51cb0b896b5d7
aluno <aluno@UCRC635.UC.ACAD>
2017-06-10 15:43:29 -0300

    Primeira versao final

--- src/jogodetiro/AppAps.java
+++ src/jogodetiro/AppAps.java
@@ -3,13 +3,15 @@ package jogodetiro;
 import jogodetiro.TiroAoAlvo;
 
 /**
+*Nomes dos integrantes do grupo.
+* 
 * Davi Henrique dos Santos Silva   2016100588
 * Igor Sousa de Amorim             2016100577
 * Mateus Marques da Costa          2016101068
 * Victor Oliveira Fonseca de Melo  2016100925
 * ...
 */
-public class JogoDeTiro {
+public class AppAps {
    
     public static void main(String[] args) {
         TiroAoAlvo jogo = new TiroAoAlvo(100,5);
